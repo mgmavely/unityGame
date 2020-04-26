bf1b8ed08eb37651e474628c8485dc4ac3049553
mmavely <mgmavely@gmail.com>
2020-04-26 00:30:41 -0400

    Fixed Volume Slider Updates
    State of volume slider would not visually save after loading different stages, added script "updateSlider" to solve issue.

---
+++ MyFirstGame/Assets/Scripts/updateSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class updateSlider : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider slider;
+    public string name;
+    float temp;
+    void Awake()
+    {
+        audioMixer.GetFloat(name, out temp);
+        slider.value = temp;
+    }
+}
