3db868b0d2b33f8cf27694b8eb53e488bdcc7b46
mmavely <mgmavely@gmail.com>
2020-04-30 22:07:36 -0400

    Added clickable objects using Ray system.
    (Additionally, updated UI to support added levels)

---
+++ MyFirstGame/Assets/Scripts/Music Scripts/endMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endMusic : MonoBehaviour
+{
+    public void bwahaha ()
+	{
+        FindObjectOfType<AudioManager>().creditsMusic();
+
+    }
+}
---
+++ MyFirstGame/Assets/Scripts/objectClicker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectClicker : MonoBehaviour
+{
+    public GameObject gameManager;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+		{
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+			{
+                if (hit.transform)
+				{
+                    if (hit.transform.gameObject.tag == "Clickable")
+                    {
+                        Destroy(hit.transform.gameObject);
+                        FindObjectOfType<AudioManager>().Play("coinUp");
+                        FindObjectOfType<GameManager>().coinCol();
+                    }
+				}
+			}
+		}
+    }
+}
